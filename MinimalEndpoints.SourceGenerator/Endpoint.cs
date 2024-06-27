4f6df7e97aee47ed5d44cf3cc80ac7eb75332866
Stavros Stergios <stavros.stergios@peoplecert.org>
2024-06-28 01:59:13 +0300

    Added support for non static endpoints

--- MinimalEndpoints.SourceGenerator/Endpoint.cs
+++ MinimalEndpoints.SourceGenerator/Endpoint.cs
@@ -1,14 +1,21 @@
-﻿using System.Text;
+﻿using Microsoft.CodeAnalysis;
+using System.Text;
 
 namespace MinimalEndpoints.SourceGenerator;
 
 internal sealed record Endpoint
 {
+    public bool RequireServiceRegistration => !Method.IsStatic;
+
+    public string ClassName { get; set; } = null!;
+
+    public bool IsStaticClass { get; set; }
+
     public EndpointConfig Config { get; set; } = null!;
 
-    public string Template { get; set; } = string.Empty;
+    public IMethodSymbol Method { get; set; } = null!;
 
-    public string EndpointMethod { get; set; } = string.Empty;
+    public string Template { get; set; } = string.Empty;
 
     public string HttpMethod { get; set; } = string.Empty;
 
@@ -16,13 +23,37 @@ internal sealed record Endpoint
 
     public bool HasConfiguration { get; set; }
 
+    public string ConfigureMethodName { get; set; } = null!;
+
     public EndpointGroup? Group { get; set; }
 
-    public void AppenEndpointString(StringBuilder sb, string endpointParent, string variableName)
+
+    public void AppendEndpointString(StringBuilder sb, string endpointParent, string variableName)
     {
         sb.Append("        ")
-            .Append($"var {variableName} = {endpointParent}")
-            .Append($".MapMethods(\"{Template}\", new[] {{\"{HttpMethod}\"}}, {Namespace}.{EndpointMethod})");
+            .Append($"var {variableName} = {endpointParent}");
+
+        if (Method.IsStatic is true)
+        {
+            sb.Append($".MapMethods(\"{Template}\", new[] {{\"{HttpMethod}\"}}, {Method.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)})");
+        }
+        else
+        {
+            var asyncKeyword = Method.ReturnType.ToDisplayString().StartsWith(TaskBaseType) ? "async" : null;
+
+            var handlersSignature = Method.Parameters.Length is > 0
+                ? string.Join(", ", Method.Parameters.Select(p => $"{p.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)} {p.Name}")) + ", " // also include the comma 
+                : string.Empty;
+
+
+            sb.Append($$"""
+                .MapMethods("{{Template}}", new[] {"{{HttpMethod}}"}, {{asyncKeyword}} ({{handlersSignature}} {{ClassName}} handler) =>
+                        {
+                            {{HandlerCall()}}
+                        })
+                """);
+        }
+
 
         if (Config.Validator is { })
         {
@@ -54,9 +85,43 @@ internal sealed record Endpoint
         {
             sb.AppendLine()
                 .Append("        ")
-                .Append($"{Namespace}.Configure({variableName});");
+                .Append($"{Namespace}.{ConfigureMethodName}({variableName});");
         }
 
         sb.AppendLine();
+    }
+
+
+    const string TaskBaseType = "System.Threading.Tasks.Task";
+
+    private string HandlerCall()
+    {
+        if (Method.ReturnsVoid)
+        {
+            return CreateHandlerCallString();
+        }
+        else
+        {
+            var returnType = Method.ReturnType.ToDisplayString();
+
+            bool isTask = returnType.StartsWith(TaskBaseType);
+
+            bool hasTaskResult = isTask && returnType.Length > TaskBaseType.Length && returnType[TaskBaseType.Length] == '<' && returnType.EndsWith(">");
+
+            if (isTask)
+            {
+                return hasTaskResult
+                    ? $"return await {CreateHandlerCallString()}"
+                    : $"await {CreateHandlerCallString()}";
+            }
+
+            return $"return {CreateHandlerCallString()}";
+        }
+
+
+        string CreateHandlerCallString()
+        {
+            return $"handler.{Method.Name}({string.Join(", ", Method.Parameters.Select(e => e.Name))});";
+        }
     }
 }
